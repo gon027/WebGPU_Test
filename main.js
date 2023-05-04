307cf6a7af8e0943f018e6d7929b3be5178fd084
ゴン <gon27.tk08@gmail.com>
2023-05-05 00:24:12 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var gl_matrix_1 = require("gl-matrix");
+var a = gl_matrix_1.glMatrix;
+console.log(a);
+console.log(gl_matrix_1.mat4);
+var e = gl_matrix_1.mat4.create();
+console.log(e);
---
+++ main.ts
@@ -0,0 +1,10 @@
+"use strict";
+
+import { glMatrix, mat4 } from "gl-matrix"
+
+const a = glMatrix;
+console.log(a);
+
+console.log(mat4);
+const e = mat4.create();
+console.log(e);
